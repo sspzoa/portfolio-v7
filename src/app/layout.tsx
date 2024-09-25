57014fac2d013a044fdf837fd9291f7329418d82
Seungpyo Suh <me@sspzoa.io>
2024-09-25 16:00:11 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ko">
-      <body className={WantedSansVariable.className}>{children}</body>
+      <body className={WantedSansVariable.variable}>{children}</body>
     </html>
   );
 }
