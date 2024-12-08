830e802bc1f42c000a7b77170b51c2086c110615
Seungpyo Suh <me@sspzoa.io>
2024-12-09 00:46:16 +0900

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,24 +14,6 @@ export default function Footer() {
         sspzoa
       </Link>
       . All rights reserved.
-      <br />
-      Powered by{' '}
-      <Link
-        className="ease-in-out duration-300 hover:opacity-50"
-        href="https://www.figma.com/community/file/1396574400107385980"
-        target="_blank"
-        rel="noreferrer noopener">
-        <strong>단아</strong>
-      </Link>
-      . / Inspired by{' '}
-      <Link
-        className="ease-in-out duration-300 hover:opacity-50"
-        href="https://github.com/krisamin"
-        target="_blank"
-        rel="noreferrer noopener">
-        <strong>noViceMin</strong>
-      </Link>
-      .
     </span>
   );
 }
