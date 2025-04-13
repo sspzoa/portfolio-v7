53c3afbb13cdd7db30851e0c8732ee36ba1995eb
Seungpyo Suh <me@sspzoa.io>
2025-04-13 15:53:05 +0900

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type {Metadata} from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import RootWrapper from '@/components/RootWrapper';
 import type React from 'react';
 import {Analytics} from "@vercel/analytics/react";
-
-const WantedSansVariable = localFont({
-  src: [
-    {
-      path: './fonts/WantedSansVariable.woff2',
-    },
-  ],
-  variable: '--font-WantedSansVariable',
-});
 
 export const metadata: Metadata = {
   title: "sspzoa's Portfolio",
@@ -25,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ko">
-    <body className={`${WantedSansVariable.variable} antialiased`}>
+    <body className='antialiased'>
     <Analytics />
       <RootWrapper>{children}</RootWrapper>
     </body>
